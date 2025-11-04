b5f8703ad889419a1f7262314ee8f85c0118a8e9
Yaroslav Fedyna <fedynaslava@gmail.com>
2025-11-04 12:15:09 +0200

    fix

--- app.py
+++ app.py
@@ -798,9 +798,10 @@ def create_app():
 
     return app
 
+app = create_app()
+
 
 if __name__ == "__main__":
-    app = create_app()
     with app.app_context():
         db.create_all()
     app.run(debug=True)
